d68470d1c25a703f676af927ee254478158584aa
montipallarols <montaga.pallarols@gmail.com>
2020-11-01 15:48:20 +0100

    Set up product by category endpoint

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const product = require("./models/product");
 const app = express();
 const productRouter = require("./routers/product");
+const categoryRouter = require("./routers/category")
 
 const PORT = 4000;
 
 app.use(express.json());
 app.use("/products", productRouter);
+app.use("/categories", categoryRouter)
 
 function onListen() {
   console.log("listening to:", PORT);
--- seeders/20201101132742-some-products.js
+++ seeders/20201101132742-some-products.js
@@ -11,6 +11,7 @@ module.exports = {
           priceInEuroCents: 30000,
           imageUrl:
             "https://www.pinterest.co.uk/pin/Ac68YCR0AHwP8XC14BNF4St4Sgi1HJhh7DVDcsci08Lr10K_RVxgoOs/",
+          categoryId: 1,
           createdAt: new Date(),
           updatedAt: new Date(),
         },
@@ -20,6 +21,7 @@ module.exports = {
           priceInEuroCents: 200,
           imageUrl:
             "https://www.prozis.com/us/en/prozis/15-x-mini-zero-milk-chocolate-10-g",
+          categoryId: 2,
           createdAt: new Date(),
           updatedAt: new Date(),
         },
@@ -29,6 +31,7 @@ module.exports = {
           priceInEuroCents: 2000,
           imageUrl:
             "https://www.penguinrandomhouse.com/books/158322/harry-potter-and-the-deathly-hallows-by-j-k-rowling/9780739360385/",
+          categoryId: 3,
           createdAt: new Date(),
           updatedAt: new Date(),
         },
@@ -38,6 +41,7 @@ module.exports = {
           priceInEuroCents: 100000,
           imageUrl:
             "https://www.mediamarkt.nl/nl/product/_apple-iphone-11-64-gb-wit-1637524.html",
+            categoryId: 4,
           createdAt: new Date(),
           updatedAt: new Date(),
         },
